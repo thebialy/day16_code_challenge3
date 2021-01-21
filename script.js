808dd9afa299d27fe9781fcdcadd72d789972fa7
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-21 09:44:43 -0600

    challenge 3 started

---
+++ script.js
@@ -0,0 +1,7 @@
+/////////////////////////////////
+// freeCodeCamp JS Challenges //
+///////////////////////////////
+
+// Challenge 1
+// Chunky Monkey (write a function that splits an array into groups the length of size and returns them as a 2 dimensional array)
+
