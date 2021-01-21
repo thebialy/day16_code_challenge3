e048a119b98c3d4b11df7c38dfd879debb0439c0
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-21 10:11:45 -0600

    finished challenege 1 chunky monkey

--- script.js
+++ script.js
@@ -5,3 +5,13 @@
 // Challenge 1
 // Chunky Monkey (write a function that splits an array into groups the length of size and returns them as a 2 dimensional array)
 
+const  chunkArrayInGroups = (arr, size) => {
+    let groupArr = []
+    while (arr.length > 0) {
+        groupArr.push(arr.slice(0, size))
+        arr = arr.slice(size)
+    }
+    console.log(groupArr)
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
