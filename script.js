ea043c1ef0ae1814dbb2eba414dacdb908e11db8
thebialy <67696052+thebialy@users.noreply.github.com>
2021-05-04 17:16:24 -0500

    commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // freeCodeCamp JS Challenges //
 ///////////////////////////////
 
-// Challenge 1
+// Challenge 1 //
 // Chunky Monkey (write a function that splits an array into groups the length of size and returns them as a 2 dimensional array)
 
 const  chunkArrayInGroups = (arr, size) => {
