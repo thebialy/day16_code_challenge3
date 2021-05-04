04bd5f10f2aa79fd0d58787d74afbe2c0fd5896b
thebialy <67696052+thebialy@users.noreply.github.com>
2021-05-04 17:19:03 -0500

    commit

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const bouncer = (arr) => {
 bouncer([7, "ate", "", false, 9])
 // Expected Output [7, "ate", 9]
 
-// Challenge 5
+// Challenge 5 //
 // Seek and Destroy (You will be provided with an initial array, followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments)
 
 const destroyer = (arr) => {
